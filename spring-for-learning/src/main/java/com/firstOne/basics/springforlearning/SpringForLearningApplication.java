f4d9060e5be0011b7186d9cb9465bb3807e85ea8
PC <ganesh.sadashiv-kaladagi@capgemini.com>
2021-05-11 16:42:10 +0530

    SPRING

---
+++ spring-for-learning/src/main/java/com/firstOne/basics/springforlearning/SpringForLearningApplication.java
@@ -0,0 +1,13 @@
+package com.firstOne.basics.springforlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForLearningApplication.class, args);
+	}
+
+}
---
+++ spring-for-learning/src/test/java/com/firstOne/basics/springforlearning/SpringForLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstOne.basics.springforlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
